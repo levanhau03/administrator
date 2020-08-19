9d649638d19b97b2945d73780f946590b284e667
levanhau03 <nhatki888@gmail.com>
2020-08-19 13:05:38 +0700

    Update knockout.php

--- src/lang/vi/knockout.php
+++ src/lang/vi/knockout.php
@@ -13,12 +13,16 @@ return array(
     */
 
     'delete_active_item' => 'Bạn chắc muốn xóa phần tử này? Đây là thao tác không thể hồi phục được.',
+    'delete_items' => 'Bạn có chắc chắn muốn xóa các mục nhập này không? Không thể hoàn tác thao tác này.',	
     'saving'             => 'Đang lưu...',
     'saved'              => 'Lưu xong.',
     'deleting'           => 'Đang xóa...',
+    'delete'             => 'Xoá',
+    'cancel'             => 'Hủy bỏ',	
     'deleted'            => 'Đã xóa.',
-    'character_left'     => ' ký tự trái',
-    'characters_left'    => ' các ký tự trái',
+    'delete_failed'      => 'Không xóa được mục nhập',	
+    'character_left'     => ' ký tự còn lại',
+    'characters_left'    => ' ký tự còn lại',
     'no_results'         => 'Không tìm thấy kết qủa phù hợp',
     'select_options'     => 'Chọn vài lựa chọn',
 
